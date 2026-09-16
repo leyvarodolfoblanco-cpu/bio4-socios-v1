9e56a912f0425759dea7ada28efd888c9e438d4c
leyvarodolfoblanco-cpu <leyvarodolfoblanco@gmail.com>
2026-09-16 17:00:51 -0600

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,606 @@
+// Public allowlist only. Generated from the master dated 2026-09-16.
+export const products=[
+  {
+    "id": "B4-GANOCONGRUENCIA-01",
+    "name": "Gano Congruencia",
+    "category": "SUPLEMENTOS",
+    "presentation": "30 sobres, 600 g",
+    "price": 1857,
+    "ingredients": "Linaza, inulina, amaranto, avena, nopal, Ganoderma lucidum, Hericium erinaceus (melena de león), tila, ácido glutámico, carbón, 7 azahares, valeriana, calcio de coral, alga chlorella, complejo B, semilla de uva.",
+    "image": "assets/B4-GANOCONGRUENCIA-01.png"
+  },
+  {
+    "id": "B4-GANOCONGRUENCIA-02",
+    "name": "Gano Congruencia Hogar",
+    "category": "SUPLEMENTOS",
+    "presentation": "720 g",
+    "price": 1857,
+    "ingredients": null,
+    "image": "assets/B4-GANOCONGRUENCIA-02.png"
+  },
+  {
+    "id": "B4-BIOCON-01",
+    "name": "Bio.Con Tamarindo",
+    "category": "SUPLEMENTOS",
+    "presentation": "600 g",
+    "price": 1156,
+    "ingredients": "Linaza, inulina, amaranto, avena, nopal, 7 azahares, calcio de coral, probióticos, pingüica, semilla de uva.",
+    "image": "assets/B4-BIOCON-01.png"
+  },
+  {
+    "id": "B4-BIOCON-02",
+    "name": "Bio.Con Ciruela",
+    "category": "SUPLEMENTOS",
+    "presentation": "600 g",
+    "price": 1156,
+    "ingredients": "Linaza, inulina, amaranto, avena, nopal, 7 azahares, calcio de coral, probióticos, pingüica, semilla de uva.",
+    "image": null
+  },
+  {
+    "id": "B4-BIOGRU-01",
+    "name": "Bio.Gru",
+    "category": "SUPLEMENTOS",
+    "presentation": "60 cápsulas",
+    "price": 781,
+    "ingredients": "Calcio de coral y alga chlorella.",
+    "image": "assets/B4-BIOGRU-01.png"
+  },
+  {
+    "id": "B4-BIOEN-01",
+    "name": "Bio.En",
+    "category": "SUPLEMENTOS",
+    "presentation": "130 g",
+    "price": 679,
+    "ingredients": "Ganoderma lucidum, Dioscorea villosa (wild yam).",
+    "image": "assets/B4-BIOEN-01.png"
+  },
+  {
+    "id": "B4-BIOCIA-01",
+    "name": "Bio.Cia",
+    "category": "SUPLEMENTOS",
+    "presentation": "90 cápsulas",
+    "price": 709,
+    "ingredients": "Complejo B, ácido glutámico, tila, valeriana, rhodiola, flor de azahar, carbón vegetal.",
+    "image": "assets/B4-BIOCIA-01.png"
+  },
+  {
+    "id": "B4-BIOFLEX-01",
+    "name": "Bio.Flex",
+    "category": "SUPLEMENTOS",
+    "presentation": "60 cápsulas",
+    "price": 709,
+    "ingredients": "Cartílago de tiburón, uña de gato, glucosamina, condroitina y colágeno hidrolizado.",
+    "image": "assets/B4-BIOFLEX-01.png"
+  },
+  {
+    "id": "B4-ETERNAL-01",
+    "name": "Eternal",
+    "category": "SUPLEMENTOS",
+    "presentation": "90 cápsulas",
+    "price": 765,
+    "ingredients": "NAD+, NMN, resveratrol, magnesio marino, extracto de semilla de uva, citrato de magnesio, coenzima Q10, vitamina E, vitamina D3 y té verde.",
+    "image": "assets/B4-ETERNAL-01.png"
+  },
+  {
+    "id": "B4-BIOFORCE-01",
+    "name": "Bio.Force",
+    "category": "SUPLEMENTOS",
+    "presentation": "90 cápsulas",
+    "price": 709,
+    "ingredients": "Noni, Equinácea purpúrea, polen, ginseng, jalea real y ginkgo biloba.",
+    "image": "assets/B4-BIOFORCE-01.png"
+  },
+  {
+    "id": "B4-BIOLYBER-01",
+    "name": "Bio.Lyber",
+    "category": "SUPLEMENTOS",
+    "presentation": "60 cápsulas",
+    "price": 709,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-TODAY-01",
+    "name": "Today",
+    "category": "SUPLEMENTOS",
+    "presentation": "90 cápsulas",
+    "price": 814,
+    "ingredients": "Saw palmetto, ginseng panax, uña de gato, Ganoderma lucidum, hongo Eryngii, ginkgo biloba, damiana.",
+    "image": "assets/B4-TODAY-01.png"
+  },
+  {
+    "id": "B4-SUPERBIO-01",
+    "name": "Super Bio",
+    "category": "SUPLEMENTOS",
+    "presentation": "120 tabletas",
+    "price": 565,
+    "ingredients": "Equinácea purpúrea, calcio de coral, ácido glutámico, B1 tiamina, B2 riboflavina, B3 niacina, B6 piridoxina, B9 ácido fólico, B12 cianocobalamina, vitamina E, vitamina C, zinc, magnesio, vitamina D3.",
+    "image": "assets/B4-SUPERBIO-01.png"
+  },
+  {
+    "id": "B4-BIOMIEL-01",
+    "name": "Bio.Miel",
+    "category": "SUPLEMENTOS",
+    "presentation": "546 g",
+    "price": 776,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-BIOFIT-01",
+    "name": "Bio.Fit",
+    "category": "SUPLEMENTOS",
+    "presentation": "600 ml",
+    "price": 767,
+    "ingredients": "Clorofila cúprica inglesa, hierbabuena y menta piperita.",
+    "image": "assets/B4-BIOFIT-01.png"
+  },
+  {
+    "id": "B4-4DXT-XTI-01",
+    "name": "4DXT XTI",
+    "category": "SUPLEMENTOS",
+    "presentation": "90 cápsulas",
+    "price": 817,
+    "ingredients": null,
+    "image": "assets/B4-4DXT-XTI-01.png"
+  },
+  {
+    "id": "B4-4DXT-ANTIOX-01",
+    "name": "4DXT Antiox",
+    "category": "SUPLEMENTOS",
+    "presentation": "30 sobres",
+    "price": 1529,
+    "ingredients": "Aminoácidos, vitaminas, GABA, Ganoderma lucidum, té verde, asaí berry, ginseng panax, guaraná.",
+    "image": "assets/B4-4DXT-ANTIOX-01.png"
+  },
+  {
+    "id": "B4-COLAGENO-GEL-01",
+    "name": "Colágeno gel",
+    "category": "SUPLEMENTOS",
+    "presentation": "20 sobres",
+    "price": 1088,
+    "ingredients": "Colágeno hidrolizado, moringa, mora azul y vitamina E.",
+    "image": "assets/B4-COLAGENO-GEL-01.png"
+  },
+  {
+    "id": "B4-GANOHE-01",
+    "name": "Gano He",
+    "category": "SUPLEMENTOS",
+    "presentation": "20 ml",
+    "price": 583,
+    "ingredients": "Ganoderma lucidum y Hericium erinaceus (melena de león).",
+    "image": "assets/B4-GANOHE-01.png"
+  },
+  {
+    "id": "B4-BIOPROPOLEO-01",
+    "name": "Bio.Propóleo",
+    "category": "SUPLEMENTOS",
+    "presentation": "22 ml",
+    "price": 445,
+    "ingredients": "Propóleo , Ganoderma lucidum, menta.",
+    "image": "assets/B4-BIOPROPOLEO-01.png"
+  },
+  {
+    "id": "B4-KENKO-TODAY-01",
+    "name": "Kenko Today Biopack",
+    "category": "SUPLEMENTOS",
+    "presentation": "210 g",
+    "price": 1669,
+    "ingredients": null,
+    "image": "assets/B4-KENKO-TODAY-01.png"
+  },
+  {
+    "id": "B4-KENKO-TODAY-02",
+    "name": "Kenko Today",
+    "category": "SUPLEMENTOS",
+    "presentation": "25 sobres",
+    "price": 876,
+    "ingredients": "Ganoderma lucidum, tongkat ali, ginseng.",
+    "image": null
+  },
+  {
+    "id": "B4-SATTVA-01",
+    "name": "Sattva Ceutica",
+    "category": "ALIMENTOS",
+    "presentation": "30 sobres de 15 g",
+    "price": 1541,
+    "ingredients": "Suero de leche, L-glutatión, colágeno hidrolizado, Shiitake, Pleurotus ostreatus, Ganoderma lucidum, creatina, calcio de coral, inulina, lactobacilos.",
+    "image": "assets/B4-SATTVA-01.png"
+  },
+  {
+    "id": "B4-KENKO-CAFE-01",
+    "name": "Kenko Café",
+    "category": "ALIMENTOS",
+    "presentation": "25 sobres",
+    "price": 757,
+    "ingredients": "Café de altura y Ganoderma lucidum.",
+    "image": "assets/B4-KENKO-CAFE-01.png"
+  },
+  {
+    "id": "B4-KENKO-CAFE-02",
+    "name": "Kenko Café Frasco",
+    "category": "ALIMENTOS",
+    "presentation": "135 g",
+    "price": 857,
+    "ingredients": "Café de altura y Ganoderma lucidum.",
+    "image": "assets/B4-KENKO-CAFE-02.png"
+  },
+  {
+    "id": "B4-KENKO-CAFE-03",
+    "name": "Kenko Café Biopack",
+    "category": "ALIMENTOS",
+    "presentation": "210 g",
+    "price": 922,
+    "ingredients": null,
+    "image": "assets/B4-KENKO-CAFE-03.png"
+  },
+  {
+    "id": "B4-KENKO-OLLA-01",
+    "name": "Kenko Café de Olla",
+    "category": "ALIMENTOS",
+    "presentation": "20 sobres",
+    "price": 853,
+    "ingredients": "Café de altura, Ganoderma lucidum, canela, azúcar mascabado.",
+    "image": "assets/B4-KENKO-OLLA-01.png"
+  },
+  {
+    "id": "B4-BIOFLAX-01",
+    "name": "Bio Flax Molida",
+    "category": "ALIMENTOS",
+    "presentation": "600 g",
+    "price": 478,
+    "ingredients": "Linaza canadiense molida.",
+    "image": "assets/B4-BIOFLAX-01.png"
+  },
+  {
+    "id": "B4-BIOFLAX-02",
+    "name": "Bio Flax CH",
+    "category": "ALIMENTOS",
+    "presentation": "600 g",
+    "price": 478,
+    "ingredients": "Linaza canadiense entera y chía.",
+    "image": "assets/B4-BIOFLAX-02.png"
+  },
+  {
+    "id": "B4-GANODENT-01",
+    "name": "Gano Dent",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "130 g",
+    "price": 329,
+    "ingredients": "Sábila, Ganoderma lucidum, caléndula, Equinácea purpúrea, manzanilla, hinojo y menta.",
+    "image": "assets/B4-GANODENT-01.png"
+  },
+  {
+    "id": "B4-LADOUCHE-01",
+    "name": "La Douche",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "270 ml",
+    "price": 474,
+    "ingredients": "Ganoderma lucidum, Dioscorea villosa (wild yam), ginseng.",
+    "image": "assets/B4-LADOUCHE-01.png"
+  },
+  {
+    "id": "B4-LADOUCHE-02",
+    "name": "La Douche Íntimo",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "250 ml",
+    "price": 407,
+    "ingredients": "Ganoderma lucidum, Dioscorea villosa (wild yam), ginseng, hamamelis, romero.",
+    "image": "assets/B4-LADOUCHE-02.png"
+  },
+  {
+    "id": "B4-GANOSOAP-01",
+    "name": "Gano Soap",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "3 barras",
+    "price": 744,
+    "ingredients": "Ganoderma lucidum y glicerina.",
+    "image": "assets/B4-GANOSOAP-01.png"
+  },
+  {
+    "id": "B4-4BELLE-01",
+    "name": "4 Belle",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "50 ml",
+    "price": 884,
+    "ingredients": "Ganoderma lucidum, rosa mosqueta, baba de caracol, ácido hialurónico, colágeno, elastina, semilla de uva, caléndula, sábila, manzanilla, vitamina E.",
+    "image": "assets/B4-4BELLE-01.png"
+  },
+  {
+    "id": "B4-COLAGENO-FACIAL-01",
+    "name": "Colágeno facial",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "48 g",
+    "price": 1169,
+    "ingredients": "Colágeno hidrolizado, Ganoderma lucidum, urea, baba de caracol, ácido hialurónico.",
+    "image": "assets/B4-COLAGENO-FACIAL-01.png"
+  },
+  {
+    "id": "B4-GANOSUN-01",
+    "name": "Gano Sun",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "75 ml",
+    "price": 763,
+    "ingredients": "Ganoderma lucidum.",
+    "image": "assets/B4-GANOSUN-01.png"
+  },
+  {
+    "id": "B4-DIAMANTES-SHAMPOO-01",
+    "name": "Shampoo de Diamantes",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "270 ml",
+    "price": 426,
+    "ingredients": "Ganoderma lucidum, shiitake (Lentinula edodes), baba de caracol, aceite de argán y Dioscorea villosa (wild yam.)",
+    "image": "assets/B4-DIAMANTES-SHAMPOO-01.png"
+  },
+  {
+    "id": "B4-DIAMANTES-ACOND-01",
+    "name": "Acondicionador de Diamantes",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "270 ml",
+    "price": 368,
+    "ingredients": "Ganoderma lucidum, Shiitake (Lentinula edodes) y baba de caracol.",
+    "image": "assets/B4-DIAMANTES-ACOND-01.png"
+  },
+  {
+    "id": "B4-SHII-DESMAQUILLANTE-01",
+    "name": "Desmaquillante Shii",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "75 ml",
+    "price": 420,
+    "ingredients": "Ganoderma lucidum y Shiitake (Lentínula edodes).",
+    "image": "assets/B4-SHII-DESMAQUILLANTE-01.png"
+  },
+  {
+    "id": "B4-SHII-TONICO-01",
+    "name": "Tónico Shii",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "120 ml",
+    "price": 410,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-EXFOLIANTE-01",
+    "name": "Exfoliante té verde",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "35 ml",
+    "price": 456,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-SHII-MANOS-01",
+    "name": "Shii Crema para Manos",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "114 g",
+    "price": 448,
+    "ingredients": "Shiitake (Lentínula edodes), baba de caracol, vitamina E, miel de abeja, manzanilla.",
+    "image": "assets/B4-SHII-MANOS-01.png"
+  },
+  {
+    "id": "B4-SHII-SEDA-01",
+    "name": "Emulsión Toque de Seda",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "300 g",
+    "price": 469,
+    "ingredients": "Ganoderma lucidum, Shiitake (Lentínula edodes) y baba de caracol.",
+    "image": "assets/B4-SHII-SEDA-01.png"
+  },
+  {
+    "id": "B4-SHII-TOMILLO-01",
+    "name": "Shii Tomillo",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "90 g",
+    "price": 662,
+    "ingredients": "Shiitake (Lentinula edodes), tomillo, árnica, castaño de indias y bardana.",
+    "image": "assets/B4-SHII-TOMILLO-01.png"
+  },
+  {
+    "id": "B4-4DXT-KUUL-01",
+    "name": "4DXT Ku'ul",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "114 g",
+    "price": 404,
+    "ingredients": "Ganoderma lucidum, alcanfor, árnica, eucalipto, enebro, mentol y aloe vera.",
+    "image": "assets/B4-4DXT-KUUL-01.png"
+  },
+  {
+    "id": "B4-MINICABINA-01",
+    "name": "Bolsa Mini Cabina en Casa",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "Kit",
+    "price": 5066,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-BIOCRISTAL-01",
+    "name": "Bio Cristal",
+    "category": "ECOLOGÍA",
+    "presentation": "250 ml",
+    "price": 524,
+    "ingredients": "Cristal natural.",
+    "image": "assets/B4-BIOCRISTAL-01.png"
+  },
+  {
+    "id": "B4-BIOCRISTAL-02",
+    "name": "Repuesto Cristal",
+    "category": "ECOLOGÍA",
+    "presentation": "250 ml",
+    "price": 306,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-BIOCLEAN-01",
+    "name": "Bio.Clean",
+    "category": "ECOLOGÍA",
+    "presentation": "90 ml",
+    "price": 220,
+    "ingredients": "Amida de coco, hidróxido de sodio, sulfato de amonio.",
+    "image": "assets/B4-BIOCLEAN-01.png"
+  },
+  {
+    "id": "B4-4SAVE-01",
+    "name": "4Save",
+    "category": "ECOLOGÍA",
+    "presentation": "1 esfera",
+    "price": 739,
+    "ingredients": "Cerámicas zeolitas e imanes.",
+    "image": "assets/B4-4SAVE-01.png"
+  },
+  {
+    "id": "B4-AHORRADOR-GASOLINA-01",
+    "name": "Ahorrador de gasolina",
+    "category": "ECOLOGÍA",
+    "presentation": "2 ahorradores",
+    "price": 527,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-COSMETIQUERA-01",
+    "name": "Cosmetiquera",
+    "category": "HERRAMIENTAS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-KIT-INICIO-01",
+    "name": "Kit inicio rápido",
+    "category": "HERRAMIENTAS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-CATALOGO-01",
+    "name": "Catálogo de productos",
+    "category": "HERRAMIENTAS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-REVISTA-01",
+    "name": "Revista científica",
+    "category": "HERRAMIENTAS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-FICHAS-01",
+    "name": "Juego de fichas técnicas",
+    "category": "HERRAMIENTAS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-PEDIDOS-01",
+    "name": "Block hojas de pedido",
+    "category": "HERRAMIENTAS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-NOTAS-VENTA-01",
+    "name": "Block de notas de venta",
+    "category": "HERRAMIENTAS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-MANUAL-SECUENCIA-01",
+    "name": "Manual Secuencia Mágica",
+    "category": "HERRAMIENTAS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-BOLSA-ECO-01",
+    "name": "Bolsa ecológica chica",
+    "category": "PUBLICITARIOS",
+    "presentation": "Chica",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-BOLSA-ECO-02",
+    "name": "Bolsa ecológica grande",
+    "category": "PUBLICITARIOS",
+    "presentation": "Grande",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-SHAKER-BIO4-01",
+    "name": "Shaker Bio4",
+    "category": "PUBLICITARIOS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-SHAKER-4DXT-01",
+    "name": "Shaker 4DXT",
+    "category": "PUBLICITARIOS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-VASO-KENKO-01",
+    "name": "Vaso de Kenko Café",
+    "category": "PUBLICITARIOS",
+    "presentation": "Consulta la presentación",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-PROTECTOR-VASO-01",
+    "name": "Protector de calor vaso Kenko c/5",
+    "category": "PUBLICITARIOS",
+    "presentation": "c/5",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  },
+  {
+    "id": "B4-CEUTICA4-01",
+    "name": "Ceutica4 Natural",
+    "category": "SUPLEMENTOS",
+    "presentation": "500 g, sabor natural",
+    "price": null,
+    "ingredients": null,
+    "image": "assets/B4-CEUTICA4-01.png"
+  },
+  {
+    "id": "B4-GANOSUN-02",
+    "name": "Gano Sun sobres",
+    "category": "CUIDADO PERSONAL",
+    "presentation": "15 sobres de 5 g",
+    "price": null,
+    "ingredients": null,
+    "image": null
+  }
+];
